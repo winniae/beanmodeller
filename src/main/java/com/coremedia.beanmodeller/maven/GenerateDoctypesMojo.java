503ea99c26e69bf94ad4f5b39707a50671c6e11c
Andris Ratas <andris.ratas@coremedia.com>
2011-02-07 15:37:15 +0100

    WIP: recovery of GenerateDoctypesMojo

--- src/main/java/com/coremedia.beanmodeller/maven/GenerateDoctypesMojo.java
+++ src/main/java/com/coremedia.beanmodeller/maven/GenerateDoctypesMojo.java
@@ -1,24 +1,13 @@
 package com.coremedia.beanmodeller.maven;
 
-import com.coremedia.beanmodeller.annotations.ContentBean;
-import com.coremedia.beanmodeller.processors.ContentBeanAnalyzationException;
 import com.coremedia.beanmodeller.processors.ContentBeanAnalyzerException;
 import com.coremedia.beanmodeller.processors.analyzator.ContentBeanAnalyzator;
 import com.coremedia.beanmodeller.processors.doctypegenerator.DocTypeMarshaler;
 import com.coremedia.beanmodeller.processors.doctypegenerator.DocTypeMarshalerException;
-import com.coremedia.schemabeans.DocType;
-import com.coremedia.schemabeans.DocumentTypeModel;
-import com.coremedia.schemabeans.ObjectFactory;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
-import org.springframework.core.type.filter.AnnotationTypeFilter;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 
@@ -32,30 +21,35 @@ public class GenerateDoctypesMojo extends AbstractMojo {
 
   /**
    * Path for generating and reading JAXB property beans.
+   *
    * @parameter expression="${beans.src.path}" default-value="com.coremedia.schemabeans"
    */
   private Object beanSrcPath;
 
   /**
    * Path for searching abstract content beans.
+   *
    * @parameter expression="${abstract.bean.path}" default-value="."
    */
   private String abstractBeanPath;
 
   /**
    * Default length for string properties
+   *
    * @parameter default-value="32"
    */
   private Integer propertyDefaultStringLength;
 
   /**
    * Minimal number of items in LinkList
+   *
    * @parameter default-value="0"
    */
   private Integer propertyDefaultLinkListMin;
 
   /**
    * Maximum number of items in LinkList
+   *
    * @parameter default-value="1000000"
    */
   private Integer propertyDefaultLinkListMax;
@@ -83,32 +77,6 @@ public class GenerateDoctypesMojo extends AbstractMojo {
     catch (DocTypeMarshalerException e) {
       getLog().error("Error marshaling doctype ", e);
     }
-
-
-/*    // Find annotated beans
-ClassPathScanningCandidateComponentProvider
-scanner = new ClassPathScanningCandidateComponentProvider(false);
-scanner.addIncludeFilter(new AnnotationTypeFilter(ContentBean.class));
-for (BeanDefinition bd : scanner.findCandidateComponents("com.coremedia.testcontentbeans")) {
-    System.out.println("Found annotated class marked as contentbean: " + bd.getBeanClassName());
-}
-
-// Create Doctype xml
-ObjectFactory of = new ObjectFactory();
-DocumentTypeModel documentTypeModel = of.createDocumentTypeModel();
-
-DocType docType = of.createDocType();
-docType.setName("CMObject");
-
-documentTypeModel.getXmlGrammarOrXmlSchemaOrDocType().add(docType);
-
-try {
-    JAXBContext jc = JAXBContext.newInstance("com.coremedia.schemabeans");
-    Marshaller m = jc.createMarshaller();
-    m.marshal( documentTypeModel, System.out );
-} catch (JAXBException e) {
-    e.printStackTrace();
-}*/
 
   }
 
